6be1edca355d81bf45d9dc9d90c8bc9eb16e25fd
Keijiro Takahashi <keijiro@X1-Turbo.jt.unity3d.com>
2015-01-07 12:58:35 +0900

    Initial commit.

---
+++ Assets/ChromaMip/Editor/ChromaMipProcessor.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+class ChromaMipProcessor : AssetPostprocessor
+{
+    static float RGB_Y(Color rgb)
+    {
+        return 0.299f * rgb.r + 0.587f * rgb.g + 0.114f * rgb.b;
+    }
+
+    static float RGB_Cb(Color rgb)
+    {
+        return -0.168736f * rgb.r -0.331264f * rgb.g + 0.5f * rgb.b;
+    }
+
+    static float RGB_Cr(Color rgb)
+    {
+        return 0.5f * rgb.r - 0.418688f * rgb.g - 0.081312f * rgb.b;
+    }
+
+    static float RGB_Ya(Color rgb)
+    {
+        if (rgb.a < 0.5f)
+            return 0;
+        else
+            return RGB_Y(rgb) * 255 / 256 + 1.0f / 256;
+    }
+
+    void OnPreprocessTexture()
+    {
+        var importer = assetImporter as TextureImporter;
+
+        if (!assetPath.EndsWith("CM.png")) return;
+
+        importer.textureType = TextureImporterType.GUI;
+        importer.textureFormat = TextureImporterFormat.RGBA32;
+        importer.mipmapEnabled = true;
+    }
+
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        var importer = assetImporter as TextureImporter;
+
+        if (!assetPath.EndsWith("CM.png")) return;
+
+        var hasAlpha = importer.DoesSourceTextureHaveAlpha();
+
+        var tw = texture.width;
+        var th = texture.height;
+
+        var pixels = texture.GetPixels(0);
+        var index = 0;
+
+        if (hasAlpha)
+        {
+            for (var iy = 0; iy < th; iy++)
+                for (var ix = 0; ix < tw; ix++, index++)
+                    pixels[index].a = RGB_Ya(pixels[index]);
+        }
+        else
+        {
+            for (var iy = 0; iy < th; iy++)
+                for (var ix = 0; ix < tw; ix++, index++)
+                    pixels[index].a = RGB_Y(pixels[index]);
+        }
+
+        texture.SetPixels(pixels, 0);
+
+        pixels = texture.GetPixels(1);
+        index = 0;
+
+        for (var iy = 0; iy < th / 2; iy++)
+            for (var ix = 0; ix < tw / 2; ix++, index++)
+                pixels[index].a = RGB_Cb(pixels[index]) + 0.5f;
+
+        texture.SetPixels(pixels, 1);
+
+        pixels = texture.GetPixels(2);
+        index = 0;
+
+        for (var iy = 0; iy < th / 4; iy++)
+            for (var ix = 0; ix < tw / 4; ix++, index++)
+                pixels[index].a = RGB_Cr(pixels[index]) + 0.5f;
+
+        texture.SetPixels(pixels, 2);
+
+        EditorUtility.CompressTexture(texture, TextureFormat.Alpha8, TextureCompressionQuality.Best);
+    }
+}
---
+++ Assets/ChromaMip/Shaders/ChromaMip Cutout.shader
@@ -0,0 +1,49 @@
+﻿Shader "ChromaMip/Cutout"
+{
+    Properties
+    {
+        _MainTex("Base", 2D) = "white" {}
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+
+    half3 YCbCrtoRGB(half y, half cb, half cr)
+    {
+        return half3(
+            y                 + 1.402    * cr,
+            y - 0.344136 * cb - 0.714136 * cr,
+            y + 1.772    * cb
+        );
+    }
+
+    half4 frag(v2f_img i) : SV_Target 
+    {
+        half y  = tex2Dlod(_MainTex, float4(i.uv, 0, 0)).a;
+
+        clip(y - 1.0 / 256);
+        y = (y - 1.0 / 256) * 256.0 / 255;
+
+        half cb = tex2Dlod(_MainTex, float4(i.uv, 0, 1)).a - 0.5;
+        half cr = tex2Dlod(_MainTex, float4(i.uv, 0, 2)).a - 0.5;
+
+        return half4(YCbCrtoRGB(y, cb, cr), 1);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #pragma glsl
+            ENDCG
+        }
+    } 
+}
